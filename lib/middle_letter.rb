bc48ab6b4d1f9ece7078b48bc79d8dbbd3fc74ce
Ben Stuart <benarthurstuart@hotmail.com>
2019-11-06 16:36:30 +0000

    works for even lengthed strings as well

--- lib/middle_letter.rb
+++ lib/middle_letter.rb
@@ -5,11 +5,19 @@ class Word
 
   def find_middle_letter
     if length_of_word % 2 == 1
-      @word[length_of_word/2]
+      @word[half_of_length]
+    else
+      @word[half_of_length - 1, half_of_length]
     end
   end
 
   def length_of_word
     @word.length
+  end
+
+  private
+
+  def half_of_length
+    length_of_word/2
   end
 end
--- spec/middle_letter_spec.rb
+++ spec/middle_letter_spec.rb
@@ -1,16 +1,22 @@
 require 'middle_letter'
 
 describe Word do
-  subject(:word) { described_class.new('tests') }
+  subject(:word_odd) { described_class.new('tests') }
+  subject(:word_even) { described_class.new('test') }
+  
   describe '#find_middle_letter' do
     it 'returns the middle letter for odd length string' do
-      expect(word.find_middle_letter).to eq 's'
+      expect(word_odd.find_middle_letter).to eq 's'
+    end
+
+    it 'returns the 2 middle letters for even lengthed strings' do
+      expect(word_even.find_middle_letter). to eq 'es'
     end
   end
 
   describe '#length_of_word' do
     it 'finds the length of the word' do
-      expect(word.length_of_word).to eq 5
+      expect(word_odd.length_of_word).to eq 5
     end
   end
 end
